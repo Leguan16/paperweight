24f0e245bf388350d7ef27dd4b50a4114a2a4ba4
MiniDigger | Martin <admin@benndorf.dev>
2023-10-15 11:11:10 +0200

    finish AT source application stuff

--- paperweight-lib/src/main/kotlin/io/papermc/paperweight/tasks/softspoon/ApplySourceAT.kt
+++ paperweight-lib/src/main/kotlin/io/papermc/paperweight/tasks/softspoon/ApplySourceAT.kt
@@ -25,6 +25,7 @@ import org.openrewrite.internal.ListUtils
 import org.openrewrite.java.Java17Parser
 import org.openrewrite.java.JavaIsoVisitor
 import org.openrewrite.java.tree.J
+import org.openrewrite.java.tree.J.ClassDeclaration
 import org.openrewrite.java.tree.JavaType
 import org.openrewrite.java.tree.JavaType.Primitive
 import org.openrewrite.java.tree.Space
@@ -62,7 +63,9 @@ fun main() {
 
     //val classes = listOf(root.resolve("net/minecraft/CrashReport.java"))
 
-    val parser = Java17Parser.builder().build()
+    val parser =
+        Java17Parser.builder().classpath(listOf(Path.of("D:\\IdeaProjects\\Paper\\.gradle\\caches\\paperweight\\taskCache\\codebook-minecraft.jar")))
+            .build()
     val ctx = InMemoryExecutionContext { it.printStackTrace() }
 
     val parsed = parser.parse(classes, root, ctx).toList()
@@ -107,18 +110,39 @@ class ATRecipe(val ats: AccessTransformSet) : Recipe() {
 class ATVisitor(private val ats: AccessTransformSet, private val atErrors: MutableList<String>) : JavaIsoVisitor<ExecutionContext>() {
 
     override fun visitClassDeclaration(classDecl: J.ClassDeclaration, p: ExecutionContext): J.ClassDeclaration {
-        val type = classDecl.type ?: return classDecl
-        val atClass = ats.getClass(type.fullyQualifiedName).getOrNull()
+        var actualClassDecl = classDecl
+        var innerClassName: String? = null
+        val type = actualClassDecl.type ?: return actualClassDecl
+        var className = type.fullyQualifiedName
+        var atClass = ats.getClass(className).getOrNull()
         if (atClass == null) {
-            // TODO consider inner classes
-            atErrors.add("Can't find AT for class ${type.fullyQualifiedName} (${classDecl.name.simpleName})")
-            return classDecl
+            // consider unknown types
+            className = cursor.firstEnclosing(J.CompilationUnit::class.java).packageDeclaration.packageName + "." + actualClassDecl.name.simpleName
+            atClass = ats.getClass(className).getOrNull()
+            if (atClass == null) {
+                // consider inner classes
+                for (it in actualClassDecl.body.statements) {
+                    if (it is ClassDeclaration) {
+                        atClass = ats.getClass(className + "$" + it.simpleName).getOrNull()
+                        if (atClass != null) {
+                            className = className + "$" + it.simpleName
+                            innerClassName = it.simpleName
+                            actualClassDecl = it
+                            break
+                        }
+                    }
+                }
+                if (atClass == null) {
+                    atErrors.add("Can't find AT for class ${type.fullyQualifiedName} (${actualClassDecl.name.simpleName})")
+                    return classDecl
+                }
+            }
         }
-        // store stuff so we can see that it was sued
+        // store stuff so we can see that it was used
         val fields = HashSet(atClass.fields.keys)
         val methods = HashSet(atClass.methods.keys)
 
-        val newStatements = classDecl.body.statements.map { statement ->
+        val newStatements = actualClassDecl.body.statements.map { statement ->
             when (statement) {
                 is J.VariableDeclarations -> {
                     // handle fields
@@ -130,9 +154,9 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
 
                     var resultStatement = statement
                     // handle private/package-private -> public
-                    resultStatement = handleFieldVisibility(resultStatement, atField, fieldName, type.fullyQualifiedName)
+                    resultStatement = handleFieldVisibility(resultStatement, atField, fieldName, className)
                     // handle final removal
-                    resultStatement = handleFieldFinalRemoval(resultStatement, atField, fieldName, type.fullyQualifiedName)
+                    resultStatement = handleFieldFinalRemoval(resultStatement, atField, fieldName, className)
 
                     resultStatement
                 }
@@ -145,7 +169,7 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
                         val atReturnType = atMethod.descriptor.returnType
 
                         // first check name
-                        if ( atMethod.name != methodType?.name) {
+                        if (atMethod.name != methodType?.name?.replace("<constructor>", "<init>")) {
                             return@find false
                         }
 
@@ -155,13 +179,22 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
                             return@find false
                         }
 
+                        // no need to compare return type for ctors
+                        if (atMethod.name == "<init>") {
+                            return@find true
+                        }
+
                         // then check return type
                         if (atReturnType is ObjectType && returnType is JavaType.Class) {
                             atReturnType.className.replace('/', '.') == returnType.fullyQualifiedName
-                        } else if (atReturnType is BaseType && returnType is JavaType.Primitive) {
+                        } else if (atReturnType is BaseType && returnType is Primitive) {
                             Primitive.fromKeyword(atReturnType.name.lowercase()) == returnType
-                        } else if (atReturnType is VoidType && returnType is JavaType.Primitive) {
+                        } else if (atReturnType is VoidType && returnType is Primitive) {
                             returnType == Primitive.Void
+                        } else if (atReturnType is ObjectType && returnType is JavaType.Parameterized) {
+                            atReturnType.className.replace('/', '.') == returnType.fullyQualifiedName
+                        } else if (atReturnType is ObjectType && returnType is JavaType.GenericTypeVariable) {
+                            atReturnType.className == "java/lang/Object"
                         } else {
                             false
                         }
@@ -174,9 +207,9 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
 
                     var resultStatement = statement
                     // handle private/package-private -> public
-                    resultStatement = handleMethodVisibility(resultStatement, atMethod, methodSignature, type.fullyQualifiedName)
+                    resultStatement = handleMethodVisibility(resultStatement, atMethod, methodSignature, className)
                     // handle final removal
-                    resultStatement = handleMethodFinalRemoval(resultStatement, atMethod, methodSignature, type.fullyQualifiedName)
+                    resultStatement = handleMethodFinalRemoval(resultStatement, atMethod, methodSignature, className)
 
                     resultStatement
                 }
@@ -189,14 +222,87 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
 
         // check if we have unused field ATs
         if (fields.isNotEmpty()) {
-            atErrors.add("Fields $fields in class ${type.fullyQualifiedName} don't exist!")
+            atErrors.add("Fields $fields in class $className don't exist!")
         }
         // check if we have unused method ATs
         if (methods.isNotEmpty()) {
-            atErrors.add("Methods $methods in class ${type.fullyQualifiedName} don't exist!")
+            atErrors.add("Methods $methods in class $className don't exist!")
         }
 
-        return classDecl.withBody(classDecl.body.withStatements(newStatements))
+        // fix inner classes
+        return if (className.contains("$")) {
+            classDecl.withBody(classDecl.body.withStatements(classDecl.body.statements.map {
+                if (it is ClassDeclaration && it.simpleName == innerClassName) {
+                    var resultClass: ClassDeclaration = it.withBody(it.body.withStatements(newStatements))
+                    resultClass = handleClassVisibility(resultClass, atClass, className)
+                    resultClass = handleClassFinalRemoval(resultClass, atClass, className)
+                    resultClass
+                } else {
+                    it
+                }
+            }))
+        } else {
+            var resultClass = classDecl.withBody(classDecl.body.withStatements(newStatements))
+            resultClass = handleClassVisibility(resultClass, atClass, className)
+            resultClass = handleClassFinalRemoval(resultClass, atClass, className)
+            resultClass
+        }
+    }
+
+    private fun handleClassFinalRemoval(classDecl: ClassDeclaration, atClass: AccessTransformSet.Class, className: String): ClassDeclaration {
+        if (atClass.get().final == ModifierChange.REMOVE) {
+            if (!classDecl.hasModifier(J.Modifier.Type.Final)) {
+                atErrors.add("Class $className is already not final!")
+                return classDecl
+            }
+            val newModifiers = ArrayList(classDecl.modifiers)
+            newModifiers.removeIf { it.type == J.Modifier.Type.Final }
+            return classDecl.withModifiers(newModifiers)
+        } else if (atClass.get().final != ModifierChange.NONE) {
+            atErrors.add("Unsupported modifier type for class $className: ${atClass.get().final}")
+        }
+        return classDecl
+    }
+
+    private fun handleClassVisibility(classDecl: ClassDeclaration, atClass: AccessTransformSet.Class, className: String): ClassDeclaration {
+        if (atClass.get().access == AccessChange.PUBLIC) {
+            if (classDecl.hasModifier(J.Modifier.Type.Public)) {
+                if (atClass.get().final == ModifierChange.NONE) {
+                    atErrors.add("Class $className is already public!")
+                }
+                return classDecl
+            } else if (classDecl.hasModifier(J.Modifier.Type.Private) || classDecl.hasModifier(J.Modifier.Type.Protected)) {
+                // replace the private with a new public modifier
+                return classDecl.withModifiers(classDecl.modifiers.map { mod ->
+                    if (mod.type == J.Modifier.Type.Private || mod.type == J.Modifier.Type.Protected) {
+                        J.Modifier(
+                            Tree.randomId(), mod.prefix, Markers.EMPTY, null, J.Modifier.Type.Public, emptyList()
+                        )
+                    } else {
+                        mod
+                    }
+                })
+            } else {
+                // package private -> add new modifier at the start
+                val newModifiers = ListUtils.concat(
+                    J.Modifier(
+                        Tree.randomId(), Space.EMPTY, Markers.EMPTY, null, J.Modifier.Type.Public, emptyList()
+                    ),
+                    classDecl.modifiers
+                )
+                // fix space
+                if (newModifiers.size > 1) {
+                    newModifiers[1] = newModifiers[1].withPrefix(newModifiers[1].prefix.withWhitespace(" "))
+                    return classDecl.withModifiers(newModifiers)
+                } else {
+                    return classDecl.withModifiers(newModifiers).annotations.withKind(classDecl.annotations.kind.withPrefix(Space.SINGLE_SPACE))
+                }
+            }
+        } else if (atClass.get().access != AccessChange.NONE){
+            atErrors.add("Unsupported access type for class $className: ${atClass.get().access}")
+            return classDecl
+        }
+        return classDecl
     }
 
     private fun handleMethodFinalRemoval(
@@ -227,7 +333,9 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
     ): J.MethodDeclaration {
         if (atMethod.access == AccessChange.PUBLIC) {
             if (statement.hasModifier(J.Modifier.Type.Public)) {
-                atErrors.add("Method $methodSignature in class $className is already public!")
+                if (atMethod.final == ModifierChange.NONE) {
+                    atErrors.add("Method $methodSignature in class $className is already public!")
+                }
                 return statement
             } else if (statement.hasModifier(J.Modifier.Type.Private) || statement.hasModifier(J.Modifier.Type.Protected)) {
                 // replace the private with a new public modifier
@@ -251,10 +359,29 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
                 // fix space
                 if (newModifiers.size > 1) {
                     newModifiers[1] = newModifiers[1].withPrefix(newModifiers[1].prefix.withWhitespace(" "))
+                    return statement.withModifiers(newModifiers)
+                } else {
+                    // name for ctors, return type for other
+                    return statement.withModifiers(newModifiers).withName(statement.name.withPrefix(Space.SINGLE_SPACE)).withReturnTypeExpression(statement.returnTypeExpression?.withPrefix(Space.SINGLE_SPACE))
                 }
-                return statement.withModifiers(newModifiers)
             }
-        } else if (atMethod.access != AccessChange.NONE) {
+        } else if (atMethod.access == AccessChange.PRIVATE) {
+            if (statement.hasModifier(J.Modifier.Type.Private)) {
+                if (atMethod.final == ModifierChange.NONE) {
+                    atErrors.add("Method $atMethod in class $className is already private!")
+                }
+                return statement
+            }
+        } else if (atMethod.access == AccessChange.PROTECTED) {
+            if (statement.hasModifier(J.Modifier.Type.Protected)) {
+                if (atMethod.final == ModifierChange.NONE) {
+                    atErrors.add("Method $atMethod in class $className is already protected!")
+                }
+                return statement
+            }
+        }
+
+        if (atMethod.access != AccessChange.NONE) {
             atErrors.add("Unsupported access type for method $methodSignature in class $className: ${atMethod.access}")
         }
         return statement
@@ -288,7 +415,9 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
     ): J.VariableDeclarations {
         if (atField.access == AccessChange.PUBLIC) {
             if (statement.hasModifier(J.Modifier.Type.Public)) {
-                atErrors.add("Field $fieldName in class $className is already public!")
+                if (atField.final == ModifierChange.NONE) {
+                    atErrors.add("Field $fieldName in class $className is already public!")
+                }
                 return statement
             } else if (statement.hasModifier(J.Modifier.Type.Private) || statement.hasModifier(J.Modifier.Type.Protected)) {
                 // replace the private with a new public modifier
@@ -312,10 +441,28 @@ class ATVisitor(private val ats: AccessTransformSet, private val atErrors: Mutab
                 // fix space
                 if (newModifiers.size > 1) {
                     newModifiers[1] = newModifiers[1].withPrefix(newModifiers[1].prefix.withWhitespace(" "))
+                    return statement.withModifiers(newModifiers)
+                } else {
+                    return statement.withModifiers(newModifiers).withTypeExpression(statement.typeExpression?.withPrefix(Space.SINGLE_SPACE))
                 }
-                return statement.withModifiers(newModifiers)
             }
-        } else if (atField.access != AccessChange.NONE) {
+        } else if (atField.access == AccessChange.PRIVATE) {
+            if (statement.hasModifier(J.Modifier.Type.Private)) {
+                if (atField.final == ModifierChange.NONE) {
+                    atErrors.add("Field $fieldName in class $className is already private!")
+                }
+                return statement
+            }
+        } else if (atField.access == AccessChange.PROTECTED) {
+            if (statement.hasModifier(J.Modifier.Type.Protected)) {
+                if (atField.final == ModifierChange.NONE) {
+                    atErrors.add("Field $fieldName in class $className is already protected!")
+                }
+                return statement
+            }
+        }
+
+        if (atField.access != AccessChange.NONE) {
             atErrors.add("Unsupported access type for field $fieldName in class $className: ${atField.access}")
         }
         return statement
